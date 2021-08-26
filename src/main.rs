31a81dbc8d4ef415bb5dea456a0d3fe0589f2e65
paradigmsort <paradigmsort+hades@gmail.com>
2021-08-25 17:24:58 -0700

    first working rng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use rand::SeedableRng;
+use rand_pcg::Pcg64;
+use rand::RngCore;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = Pcg64::seed_from_u64(123);
+    let value = rng.next_u32();
+    println!("{}", value);
 }
