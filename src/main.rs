73be6ae5499f072b9adbc8f6bb36f74d9d0865b7
paradigmsort <paradigmsort+hades@gmail.com>
2021-08-27 14:51:27 -0700

    first working lua

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,30 @@ mod rng;
 use rng::SggPcg;
 use rand::RngCore;
 use structopt::StructOpt;
+use rlua::Lua;
+use std::fs;
 
 #[derive(StructOpt)]
 struct Cli {
   #[structopt(short = "s", long = "seed")]
   seed: i32,
-  #[structopt(short = "n", long = "offset")]
-  offset: u32
+  #[structopt(parse(from_os_str))]
+  lua_path: std::path::PathBuf
 }
 
 fn main() {
     let args = Cli::from_args();
+    let lua = Lua::new();
     let mut rng = SggPcg::new(args.seed as u64);
-    if args.offset > 1 {
-      rng.advance((args.offset - 1).into());
-    }
-    println!("{}", rand_int(&mut rng, -2147483647, 2147483646));
+    let file = fs::read(args.lua_path).expect("unable to read file");
+    let seed = args.seed;
+    lua.context(|lua_ctx| {
+        lua_ctx.load(&file).exec().expect("unable to exec lua file");
+        lua_ctx.globals().set("RouteFinderSeed", seed);
+        lua_ctx.load(r#"RouteFinderFirstRoomChaos = PredictStartingRoomReward(RouteFinderSeed).FirstRoomChaos"#)
+            .exec().expect("unable to predict rewards!");
+        println!("{}", lua_ctx.globals().get::<_, bool>("RouteFinderFirstRoomChaos").expect("not working"));
+    })
 }
 
 fn rand_int(rng: &mut SggPcg, min: i32, max: i32) -> i32 {
