cb40caacb90703f954123ce12b16d6326a40a4a8
paradigmsort <paradigmsort+hades@gmail.com>
2021-08-28 20:40:51 -0700

    load table, load value

--- src/luabins.rs
+++ src/luabins.rs
@@ -106,3 +106,13 @@ fn load_table<'lua>(loadstate: &mut &[u8], context: Context<'lua>, err: String) 
    }
    Ok(table)
 }
+
+fn load<'lua>(loadstate: &mut &[u8], context: Context<'lua>, err: String) -> Result<Vec<Value<'lua>>, String> {
+    let num_items = read_byte(loadstate, refine(&err, "num_items"))?;
+    let mut vec = Vec::new()
+    for i in 1..num_items {
+        let value = load_value(loadstate, context, err)?;
+        vec.push(value);:w
+    }
+    Ok(vec)
+}
