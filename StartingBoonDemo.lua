1c60deae384a2663f2bb2ed26114f1f304079e76
paradigmsort <paradigmsort+hades@gmail.com>
2021-09-04 18:52:59 -0700

    split out matching utils

--- StartingBoonDemo.lua
+++ StartingBoonDemo.lua
@@ -1,62 +1,7 @@
 ParasDoorPredictions.Config.PrintNextSeed = false
 
 Import "Utils/DeepPrint.lua"
-
-function matches(requirements, candidate)
-  for k,v in pairs(requirements) do
-    if candidate[k] == nil then
-      return false
-    end
-    if type(v) == "function" then
-      if not v(candidate[k]) then
-        return false
-      end
-    elseif type(v) == "table" then
-      if not matches(v, candidate[k]) then
-        return false
-      end
-    elseif candidate[k] ~= v then
-      return false
-    end
-  end
-  return true
-end
-
-function one_matches(requirements, candidates)
-  if type(candidates) ~= "table" then
-    return false
-  end
-  for _,candidate in pairs(candidates) do
-    if matches(requirements, candidate) then
-      return true
-    end
-  end
-  return false
-end
-
-function matches_table(requirements, candidates)
-  if type(candidates) ~= "table" then
-    return false
-  end
-  for k,v in pairs(candidates) do
-    if requirements[k] == nil then
-      return false
-    end
-    if requirements[k] ~= v then
-      return false
-    end
-  end
-  return true
-end
-
-function matches_one(options, candidate)
-  for k,v in pairs(options) do
-    if v == candidate then
-      return true
-    end
-  end
-  return false
-end
+Import "Utils/Matching.lua"
 
 function PredictC2Options( roomReward )
   local oldUses = ParasDoorPredictions.CurrentUses
