3eef017d70e0d9d8174a7097c4d2ec689225bf7e
paradigmsort <paradigmsort+hades@gmail.com>
2022-01-30 13:07:59 -0800

    updated fresh file script

--- FreshFileNonsense.lua
+++ FreshFileNonsense.lua
@@ -4,6 +4,8 @@ MinSeed = 15750000
 MaxSeed = 15950000
 MinC1Uses = 27
 MaxC1Uses = 32
+MinC3Uses = 5
+MaxC3Uses = 8
 
 local C2Door = CreateC2Door({
     SecondRoomName = "RoomSimple01", -- Athena Room
@@ -73,12 +75,19 @@ function record(result)
     result.C1_Seed,
     result.C2_Seed,
     result.C3_Seed,
+    result.C4_Seed,
     result.C2_DoorRewards,
     result.C3_RoomName,
     result.C3_DoorRewards,
     result.C3_Waves[1] or "",
     result.C3_Waves[2] or "",
-    result.C3_Waves[3] or ""
+    result.C3_Waves[3] or "",
+    result.C4_RewardChosen,
+    result.C4_RoomName,
+    result.C4_DoorRewards,
+    result.C4_Waves[1] or "",
+    result.C4_Waves[2] or "",
+    result.C4_Waves[3] or ""
   }))
 end
 
@@ -86,27 +95,48 @@ for seed=MinSeed,MaxSeed do
   if seed % 10000 == 0 then
     io.stderr:write(seed, "\n")
   end
-  NextSeeds[1] = seed
   for uses=MinC1Uses,MaxC1Uses  do
+    NextSeeds[1] = seed
     RandomSynchronize(uses)
     CurrentRun = C1Run
-    local prediction = PredictLoot(C2Door)
+    local c2_prediction = PredictLoot(C2Door)
     local result = {
       C1_Seed = seed,
-      C2_Seed = prediction.Seed,
-      C2_DoorRewards = ExitRewards(prediction.NextExitRewards),
-      C3_RoomName = prediction.NextExitRewards[1].RoomName
+      C2_Seed = c2_prediction.Seed,
+      C2_DoorRewards = ExitRewards(c2_prediction.NextExitRewards),
+      C3_RoomName = c2_prediction.NextExitRewards[1].RoomName
     }
     local c3door = {
-      Room = DeepCopyTable(prediction.NextExitRewards[1].Room)
+      Room = DeepCopyTable(c2_prediction.NextExitRewards[1].Room)
     }
-    NextSeeds[1] = prediction.Seed
+    NextSeeds[1] = c2_prediction.Seed
     RandomSynchronize(6) -- uses at end of athena room
     CurrentRun = C2Run
-    local prediction = PredictLoot(c3door)
-    result.C3_Seed = prediction.Seed
-    result.C3_DoorRewards = ExitRewards(prediction.NextExitRewards)
-    result.C3_Waves = EncounterWaves(prediction.Encounter)
-    record(result)
+    local c3_prediction = PredictLoot(c3door)
+    result.C3_Seed = c3_prediction.Seed
+    result.C3_DoorRewards = ExitRewards(c3_prediction.NextExitRewards)
+    result.C3_Waves = EncounterWaves(c3_prediction.Encounter)
+    for _, exit in pairs(c3_prediction.NextExitRewards) do
+      result.C4_RoomName = exit.RoomName
+      result.C4_RewardChosen = exit.ForceLootName or exit.RewardType
+      local c4door = {
+        Room = DeepCopyTable(exit.Room)
+      }
+      local reward = {
+        Seed = c3_prediction.Seed,
+        Prediction = c3_prediction
+      }
+      CurrentRun = MoveToNextRoom(C2Run, reward, c3door)
+      PickUpReward(CurrentRun) -- c3 is always MetaProgress
+      for uses=MinC3Uses,MaxC3Uses do
+        NextSeeds[1] = c3_prediction.Seed
+        RandomSynchronize(uses)
+        local c4_prediction = PredictLoot(c4door)
+        result.C4_Seed = c4_prediction.Seed
+        result.C4_DoorRewards = ExitRewards(c4_prediction.NextExitRewards)
+        result.C4_Waves = EncounterWaves(c4_prediction.Encounter)
+        record(result)
+      end
+    end
   end
 end
