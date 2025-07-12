5bcacd8f0978789637332cb62cb08828005474b8
paradigmsort <paradigmsort+hades@gmail.com>
2025-07-12 11:42:00 -0700

    fix

--- styx.sh
+++ styx.sh
@@ -1,1 +1,1 @@
-cargo run -- FindStyx.lua --save-file ~/workspace/hades/HadesRouteManager/Routes/4Shop/styx_12/Profile4_Temp.sav --scripts-dir ~/legendary/Hades/Content/Scripts/
+cargo run -- run FindStyx.lua --save-file ~/workspace/hades/HadesRouteManager/Routes/4Shop/styx_12/Profile4_Temp.sav --scripts-dir ~/legendary/Hades/Content/Scripts/
