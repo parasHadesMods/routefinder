6e66582d9cdee25fb5f4ef3fae00d1ac96cf6d67
paradigmsort <paradigmsort+hades@gmail.com>
2025-07-19 08:49:35 -0700

    partially working

---
+++ RouteFreshFileIncrementally.lua
@@ -0,0 +1,146 @@
+Import "Utils/FindIncrementally.lua"
+-- Import "Utils/LazyDeepCopyTable.lua"
+-- DeepCopyTable = LazyDeepCopyTable
+
+local C2Door = CreateC2Door({
+    SecondRoomName = "RoomSimple01", -- Athena Room
+    SecondRoomReward = "Athena",
+    SecondRoomRewardStore = "RunProgress"
+})
+C2Door.Room.Encounter = {}
+
+local AthenaDashTrait = GetProcessedTraitData({ Unit = CurrentRun.Hero, TraitName = "AthenaRushTrait", Rarity = "Common" })
+table.insert(CurrentRun.Hero.Traits, AthenaDashTrait)
+CurrentRun.LootTypeHistory["AthenaUpgrade"] = 1
+
+if type(AthenaSeed) ~= "number" then
+  print("Invalid seed, make sure to pass --lua-var AthenaSeed=<number>")
+end
+
+if type(AthenaOffset) ~= "number" then
+  print("Invalid offset, make sure to pass --lua-var AthenaOffset=<number>")
+end
+
+NextSeeds[1] = AthenaSeed
+
+function RewardForExitRoom(exits, roomName)
+  for _, exit in pairs(exits) do
+    if exit.RoomName == roomName then
+      return exit.Reward
+    end
+  end
+end
+
+local requirements = {
+  C3 = {
+    Room = {},
+    Exit = {
+      Reward = "AresUpgrade",
+    }
+  },
+  C4 = {
+    Room = {
+      UpgradeOptions = function(options)
+        return one_matches({
+            ItemName = "AresWeaponTrait",
+            Rarity = function (rarity)
+              return matches_one({ "Rare", "Epic" }, rarity)
+            end
+        }, options)
+      end
+    },
+    Exit = {}
+  },
+  C5 = {
+    Room = {},
+    Exit = {
+      Reward = "AthenaUpgrade"
+    }
+  },
+  C6 = {
+    Room = {
+      UpgradeOptions = function(options)
+        return one_matches({
+            ItemName = "AthenaSecondaryTrait"
+        }, options)
+      end
+    },
+    Exit = {
+      RoomName = "A_MiniBoss01"
+    }
+  },
+  C7 = {
+    Room = {
+      UpgradeOptions = function(options)
+        return one_matches({
+            ItemName = "TriggerCurseTrait",
+            Rarity = "Legendary"
+        }, options)
+      end
+    },
+    Exit = {}
+  }
+}
+
+
+RandomSynchronize()
+local c2ExitRoomData = ChooseNextRoomData(CurrentRun)
+local c2ExitDoor = {
+  Room = ParasDoorPredictions.CreateRoom(CurrentRun, c2ExitRoomData, { SkipChooseReward = true, SkipChooseEncounter = true})
+}
+c2ExitDoor.Room.ChosenRewardType = ParasDoorPredictions.ChooseRoomReward(CurrentRun, c2ExitDoor.Room, "MetaProgress", {}, { PreviousRoom = C2Door.Room, Door = c2ExitDoor }) -- calls RandomSynchronize(4)
+c2ExitDoor.Room.RewardStoreName = "MetaProgress"
+
+CurrentRun.CurrentRoom = C2Door.Room
+
+local prediction = PredictLoot(c2ExitDoor) -- standing in front of c3 door, in c2
+local result_table = {}
+
+Setup(CurrentRun, c2ExitDoor, requirements, 2, 7, AthenaOffset)
+i = 0
+while i < 12 do
+  Increment()
+  i = i + 1
+end
+
+local result_table = Results()
+local min_cost = nil
+local min_display = nil
+for _, route in pairs(result_table) do
+  local display = {
+    C2 = {
+      Cast = route.C3.Uses - AthenaOffset,
+      Door = c2ExitDoor.Room.ChosenRewardType
+    },
+    C3 = {
+      Cast = route.C4.Uses - route.C3.EstimatedEndOfRoomOffset,
+      Door = "AresUpgrade"
+    },
+    C4 = {
+      Cast = route.C5.Uses - route.C4.EstimatedEndOfRoomOffset,
+      Door = RewardForExitRoom(route.C4.Exits, route.C5.RoomName),
+      Take = route.C4.UpgradeOptions[1].ItemName .. " " .. route.C4.UpgradeOptions[1].Rarity,
+    },
+    C5 = {
+      Cast = route.C6.Uses - route.C5.EstimatedEndOfRoomOffset,
+      Door = "AthenaUpgrade"
+    },
+    C6 = {
+      Cast = route.C7.Uses - route.C6.EstimatedEndOfRoomOffset,
+      Take = route.C6.UpgradeOptions[1].ItemName .. " " .. route.C6.UpgradeOptions[1].Rarity,
+      Door = RewardForExitRoom(route.C6.Exits, route.C7.RoomName)
+    },
+    C7 = {
+      Take = "TriggerCurseTrait"
+    }
+  }
+  local cost = display.C2.Cast + display.C3.Cast + display.C4.Cast + display.C5.Cast + display.C6.Cast
+  if min_cost == nil or min_cost > cost then
+    min_cost = cost
+    min_display = display
+  end
+end
+
+if min_display ~= nil then
+  deep_print(min_display)
+end
--- Utils/FindIncrementally.lua
+++ Utils/FindIncrementally.lua
@@ -1,3 +1,4 @@
+Import "Utils/FindRoute.lua"
 local state = {}
 
 -- Notation
@@ -9,6 +10,7 @@ local state = {}
 function Setup(run, door, requirements, cStart, cEnd, oStart)
     -- clear previous state
     state = {}
+    state.Increments = 0
     state.Requirements = requirements
     state.cStart = cStart
     state.cEnd = cEnd
@@ -19,6 +21,7 @@ function Setup(run, door, requirements, cStart, cEnd, oStart)
     table.insert(state.rssReached[cStart], 
         {
             Run = run,
+            Seed = NextSeeds[1],
             Door = door,
             Display = "",
             oMinimum = oStart,
@@ -27,19 +30,34 @@ function Setup(run, door, requirements, cStart, cEnd, oStart)
     )
 end
 
-function NextRooms(rCurrent)
-    return {
-        {
-            Display = rCurrent.Display .. " " .. (rCurrent.oNext - rCurrent.oMinimum),
-            oMinimum = 7,
-            oNext = 7
-        }
-    }
+function NextRooms(rCurrent, ci)
+    local results = {}
+    local requirements = state.Requirements["C" .. (ci + 1)]
+
+    NextSeeds[1] = rCurrent.Seed
+    for _, reward in pairs(PredictRoomOptions(rCurrent.Run, rCurrent.Door, { Min = rCurrent.oNext, Max = rCurrent.oNext })) do
+        if matches(requirements.Room, reward) then
+            reward.RoomName = rCurrent.Door.Room.Name
+            reward.Run = MoveToNextRoom(rCurrent.Run, reward, rCurrent.Door)
+            reward.Seed = NextSeeds[1]
+            reward.oMinimum = reward.EstimatedEndOfRoomOffset
+            reward.oNext = reward.oMinimum
+            if not requirements.SkipReward then
+                PickUpReward(reward.Run, requirements.Boon, reward)
+            end
+            for _, door in pairs(ExitDoors(reward.Run, requirements, reward)) do
+                local doorReward = DeepCopyTable(reward)
+                doorReward.Door = door
+                table.insert(results, doorReward)
+            end
+        end
+    end
+    return results
 end
 
 function IncrementChamber(ci)
     for _, room in pairs(state.rssReached[ci]) do
-        local rsNext = NextRooms(room)
+        local rsNext = NextRooms(room, ci)
         for _, rNext in pairs(rsNext) do
             table.insert(state.rssReached[ci+1], rNext)
         end
@@ -48,7 +66,22 @@ function IncrementChamber(ci)
 end
 
 function Increment()
+    state.Increments = state.Increments + 1
     for ci=state.cStart,state.cEnd do
         IncrementChamber(ci)
     end
+end
+
+function MonitorProgress()
+    if state.Increments % 10 == 0 then
+        local progress = state.Increments .. ": " .. #state.rssReached[state.cStart]
+        for ci=state.cStart+1,state.cEnd do
+            progress = progress .. " " .. #state.rssReached[ci]
+        end
+        print(progress)
+    end
+end
+
+function Results()
+    return state.rssReached[state.cEnd]
 end
